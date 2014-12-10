50ba739a1ff8ac0d492039fa07913c11dce53131
Vitor Faiante <vitorf7@gmail.com>
2014-12-10 11:42:40 +0000

    Make changes to the category and 404 pages

--- wp-content/themes/tls/category.php
+++ wp-content/themes/tls/category.php
@@ -29,8 +29,6 @@
 					get_template_part( 'content', get_post_format() );
 
 					endwhile;
-					// Previous/next page navigation.
-					twentyfourteen_paging_nav();
 
 				else :
 					// If no content, include the "No posts found" template.
