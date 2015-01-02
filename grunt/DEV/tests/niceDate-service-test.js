14626fea6ce4cd23c062108fe37b0dfdf0a21318
Dan Osborne <dan.osborne@fantasticthinking.com>
2015-01-02 11:56:36 +0000

    fixed failing unit test in search controller

---
+++ grunt/DEV/tests/niceDate-service-test.js
@@ -0,0 +1,17 @@
+describe('niceDate', function() {
+
+    beforeEach(module('tls'));
+
+    it('should convert wordpress date format', inject(function(niceDate){
+
+        var date    = "2014-12-02 10:32:36",
+            test    = niceDate.format(date),
+            conv    = "2 DECEMBER 2014",
+            match   = (test.indexOf(conv) > -1 && conv.length == test.length) ? true : false;
+
+        expect(typeof test).toBe("string");
+        expect(match).toBe(true);
+        
+    }));
+
+})
--- grunt/DEV/tests/search-controller-test.js
+++ grunt/DEV/tests/search-controller-test.js
@@ -63,16 +63,6 @@ describe('Unit: search', function() {
     //     expect(test).toBe('derp');
     // });
 
-    it('should convert wordpress date format', function(){
-
-        var date    = "2014-12-02 10:32:36",
-            test    = scope.format(date),
-            conv    = "2 DECEMBER 2014",
-            match   = (test.indexOf(conv) > -1 && conv.length == test.length) ? true : false;
-
-        expect(typeof test).toBe("string");
-        expect(match).toBe(true);
-        
-    });
+    
     
 })
