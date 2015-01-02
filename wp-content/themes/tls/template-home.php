01ef0dc0bae29d350025e763cdeb6d95e87f218c
Dan Osborne <dan.osborne@fantasticthinking.com>
2015-01-02 12:04:47 +0000

    home page tweaks

--- wp-content/themes/tls/template-home.php
+++ wp-content/themes/tls/template-home.php
@@ -7,24 +7,78 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section id="home" ng-controller="home">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<div id="banner" style="background-image:url(/wp-content/themes/tls/images/hero.jpg)">
 
-				<?php get_template_part( 'content', 'page' ); ?>
+		<div class="container">
+			
+			<div class="caption">
+				<p class="category">Memoir</p>
+				<h2>The soldier poets</h2>
+				<p class="excerpt">Does poetry carry more weight than history in the legacy of the First World War?</p>
+			</div>
+		</div>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+		<div class="gradient"></div>
 
-			<?php endwhile; // end of the loop. ?>
+	</div>
+	
+	<div class="container">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<div ng-if="columns" tls-columns="columns"></div>
 
-<?php get_sidebar(); ?>
+	</div>
+
+	<div class="grid-row" id="subscriber" ng-class="{locked:isLocked}">
+
+		<div class="container">
+		
+			<h5 class="centred-heading grid-row">Subscriber exclusive</h5>
+
+			<div class="subscribe-grid">
+				<div class="card">
+					<h3 class="futura">Archive</h3>
+					<img class="max" src="http://placehold.it/380x192">
+					<p class="padded">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+				</div>
+			</div>
+
+			<div class="subscribe-grid">				
+				<div class="card">
+					<h3 class="futura">Letters to the editor</h3>
+					<img class="max" src="http://placehold.it/380x192">
+					<p class="padded">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+				</div>
+			</div>
+
+			<div class="subscribe-grid">
+				<div class="card">
+					<h3 class="futura">NB</h3>
+					<img class="max" src="http://placehold.it/380x192">
+					<p class="padded">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+				</div>
+			</div>
+
+			<div class="subscribe-grid">
+				<div class="card">
+					<h3 class="futura">Wall street journal</h3>
+					<img class="max" src="http://placehold.it/380x192">
+					<p class="padded">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
 <?php get_footer(); ?>
