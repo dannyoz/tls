2f76f0c723e12fc4a16e74099251ba402880d88c
unknown <Silvia.Rebelo@LDNFTWD009199.5gs.local>
2015-01-06 14:00:05 +0000

    Latest Editions / Angular structure

---
+++ grunt/DEV/app/templates/latest-editions/latest-editions-controller.js
@@ -0,0 +1,8 @@
+.controller('latesteditions',['$scope','$sce','$location','$timeout','api','columns','niceDate',
+
+	function ($scope,$sce,$location,$timeout,api,columns,niceDate) {
+
+		api.getLatestEditions().then(function (result){			
+
+		})
+}])
