cd88501c30e1442e443bf6ce81604c6c8729d3cb
Dan Osborne <dan.osborne@fantasticthinking.com>
2015-01-09 13:55:49 +0000

    included top 16 articles on discover template

--- wp-content/themes/tls/template-articles-archive.php
+++ wp-content/themes/tls/template-articles-archive.php
@@ -29,21 +29,101 @@ $articles_archive = new WP_Query($articles_archive_args);
 
 	<div tls-scroll="scrollState">
 
-			<div class="container" ng-if="ready">
+		<div class="container" ng-if="ready">
 
-				<div class="grid-row" ng-if="size == 'desktop' || size == 'mobile'">
+			<div id="top-articles">
+
+				<div class="grid-row" ng-if="size == 'desktop'">
+
+					<div  class="grid-4" ng-repeat="column in topCol3">
+
+						<div class="card" ng-repeat="card in column" ng-class="{private:card.taxonomy_article_visibility[0].slug == 'private'}">
+							<h3 class="futura">
+								<a ng-attr-href="/article_section/{{card.taxonomy_article_section[0].slug}}" ng-bind-html="card.taxonomy_article_section[0].name"></a>
+								<i ng-if="card.taxonomy_article_visibility[0].slug == 'private'" class="icon icon-key"></i>
+							</h3>
+							<img class="max" ng-if="card.custom_fields.thumbnail_image_url" ng-attr-src="{{card.custom_fields.thumbnail_image_url}}" />
+							<div class="padded">
+								<h4><a ng-attr-href="{{card.url}}" ng-bind="card.title"></a></h4>
+								<p ng-bind-html="truncate(card.content)"></p>
+							</div>
+							<footer>
+								<p class="futura" ng-bind="card.author.name"></p>
+							</footer>
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="grid-row" ng-if="size == 'tablet'">
+
+					<div class="grid-6" ng-repeat="column in topCol2">
+
+						<div class="card" ng-repeat="card in column" ng-class="{private:card.taxonomy_article_visibility[0].slug == 'private'}">
+							<h3 class="futura">
+								<a ng-attr-href="/article_section/{{card.taxonomy_article_section[0].slug}}" ng-bind-html="card.taxonomy_article_section[0].name"></a>
+								<i ng-if="card.taxonomy_article_visibility[0].slug == 'private'" class="icon icon-key"></i>
+							</h3>
+							<img class="max" ng-if="card.custom_fields.thumbnail_image_url" ng-attr-src="{{card.custom_fields.thumbnail_image_url}}" />
+							<div class="padded">
+								<h4><a ng-attr-href="{{card.url}}" ng-bind="card.title"></a></h4>
+								<p ng-bind-html="truncate(card.content)"></p>
+							</div>
+							<footer>
+								<p class="futura" ng-bind="card.author.name"></p>
+							</footer>
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="grid-row" ng-if="size == 'mobile'">
+
+					<div class="grid-6" ng-repeat="column in topCol1">
+
+						<div class="card" ng-repeat="card in column" ng-class="{private:card.taxonomy_article_visibility[0].slug == 'private'}">
+							<h3 class="futura">
+								<a ng-attr-href="/article_section/{{card.taxonomy_article_section[0].slug}}" ng-bind-html="card.taxonomy_article_section[0].name"></a>
+								<i ng-if="card.taxonomy_article_visibility[0].slug == 'private'" class="icon icon-key"></i>
+							</h3>
+							<img class="max" ng-if="card.custom_fields.thumbnail_image_url" ng-attr-src="{{card.custom_fields.thumbnail_image_url}}" />
+							<div class="padded">
+								<h4><a ng-attr-href="{{card.url}}" ng-bind="card.title"></a></h4>
+								<p ng-bind-html="truncate(card.content)"></p>
+							</div>
+							<footer>
+								<p class="futura" ng-bind="card.author.name"></p>
+							</footer>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div id="infinite-scroll" ng-if="infinite">
+
+				<h5 class="centred-heading grid-row">More articles</h5>
+
+				<div class="grid-row" ng-if="size == 'desktop'">
 
 					<div  class="grid-4" ng-repeat="column in col3">
 
 						<div class="card" ng-repeat="card in column" ng-class="{private:card.taxonomy_article_visibility[0].slug == 'private'}">
 							<h3 class="futura">
-								<a ng-attr-href="{{card.url}}" ng-bind="card.title"></a>
+								<a ng-attr-href="/article_section/{{card.taxonomy_article_section[0].slug}}" ng-bind="card.taxonomy_article_section[0].title"></a>
 								<i ng-if="card.taxonomy_article_visibility[0].slug == 'private'" class="icon icon-key"></i>
 							</h3>
 							<img class="max" ng-if="card.custom_fields.thumbnail_image_url[0].length > 0" ng-attr-src="{{card.custom_fields.thumbnail_image_url[0]}}" />
-							<div class="padded" ng-bind-html="card.excerpt"></div>
+							<div class="padded">
+								<h4><a ng-attr-href="{{card.url}}" ng-bind="card.title"></a></h4>
+								<div ng-bind-html="card.excerpt"></div>
+							</div>
 							<footer>
-								<p class="futura"><a href="#" ng-bind="card.author.name"></a></p>
+								<p class="futura" ng-bind="card.author.name"></p>
 							</footer>
 						</div>
 
@@ -57,13 +137,16 @@ $articles_archive = new WP_Query($articles_archive_args);
 
 						<div class="card" ng-repeat="card in column" ng-class="{private:card.taxonomy_article_visibility[0].slug == 'private'}">
 							<h3 class="futura">
-								<a ng-attr-href="{{card.url}}" ng-bind="card.title"></a>
+								<a ng-attr-href="/article_section/{{card.taxonomy_article_section[0].slug}}" ng-bind="card.taxonomy_article_section[0].title"></a>
 								<i ng-if="card.taxonomy_article_visibility[0].slug == 'private'" class="icon icon-key"></i>
 							</h3>
 							<img class="max" ng-if="card.custom_fields.thumbnail_image_url[0].length > 0" ng-attr-src="{{card.custom_fields.thumbnail_image_url[0]}}" />
-							<div class="padded" ng-bind-html="card.excerpt"></div>
+							<div class="padded">
+								<h4><a ng-attr-href="{{card.url}}" ng-bind="card.title"></a></h4>
+								<div ng-bind-html="card.excerpt"></div>
+							</div>
 							<footer>
-								<p class="futura"><a href="#" ng-bind="card.author.name"></a></p>
+								<p class="futura" ng-bind="card.author.name"></p>
 							</footer>
 						</div>
 
@@ -71,20 +154,45 @@ $articles_archive = new WP_Query($articles_archive_args);
 
 				</div>
 
-				<div id="load-more" class="grid-row">
+				<div class="grid-row" ng-if="size == 'mobile'">
 
-					<p class="centred futura">{{loadMsg}}</p>
-					<div tls-loading="infLoading"></div>
-					<button class="clear centre" ng-if="!infinite && pageCount > 1" ng-click="loadMore();">
-						Load more <i class="icon icon-plus"></i>
-					</button>
+					<div class="grid-6" ng-repeat="column in col1">
+
+						<div class="card" ng-repeat="card in column" ng-class="{private:card.taxonomy_article_visibility[0].slug == 'private'}">
+							<h3 class="futura">
+								<a ng-attr-href="/article_section/{{card.taxonomy_article_section[0].slug}}" ng-bind="card.taxonomy_article_section[0].title"></a>
+								<i ng-if="card.taxonomy_article_visibility[0].slug == 'private'" class="icon icon-key"></i>
+							</h3>
+							<img class="max" ng-if="card.custom_fields.thumbnail_image_url[0].length > 0" ng-attr-src="{{card.custom_fields.thumbnail_image_url[0]}}" />
+							<div class="padded">
+								<h4><a ng-attr-href="{{card.url}}" ng-bind="card.title"></a></h4>
+								<div ng-bind-html="card.excerpt"></div>
+							</div>
+							<footer>
+								<p class="futura" ng-bind="card.author.name"></p>
+							</footer>
+						</div>
+
+					</div>
+
 				</div>
 
 			</div>
 
-			<div tls-loading="loading"></div>
+			<div id="load-more" class="grid-row">
+
+				<p class="centred futura">{{loadMsg}}</p>
+				<div tls-loading="infLoading"></div>
+				<button class="clear centre" ng-if="!infinite && pageCount > 1" ng-click="loadMore();">
+					Load more <i class="icon icon-plus"></i>
+				</button>
+			</div>
 
 		</div>
+
+		<div tls-loading="loading"></div>
+
+	</div>
 
 </section>
 
