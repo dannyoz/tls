742146b11f9117648b7ea0a2d00aa4a4d914d845
Silvia Rebelo <silvia.rebelo@fantasticthinking.com>
2015-01-29 16:54:41 +0000

    Improving 404 page to match designs

--- wp-content/themes/tls/searchform.php
+++ wp-content/themes/tls/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div>
+	<div class="searchform-wrapper">
 		<label class="screen-reader-text" for="s">Search:</label>
-		<input type="text" value="<?php echo get_search_query(); ?>" placeholder="Tls archive, blogs and website" name="s" id="s" />
-		<input type="submit"/>
+		<input type="text" value="<?php echo get_search_query(); ?>" placeholder="Tls archive, blogs and website" name="s" id="s" />        
+		<button type="submit" class="icon icon-search"></button>
 	</div>
 </form>
