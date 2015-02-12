e6e4c4dcf6fbc5c446942a1dc44616028622b39d
Silvia Rebelo <silvia.rebelo@fantasticthinking.com>
2015-02-12 11:34:33 +0000

    NUTLS-134 Fix for search active states not working correctly

--- grunt/DEV/app/global/ng-services/api.js
+++ grunt/DEV/app/global/ng-services/api.js
@@ -1,171 +1,174 @@
 .factory('api',['$http','$q','$timeout', function ($http,$q,$timeout){
 
-	var delay  = 1;
+    var delay  = 1;
 
-	return {
-		getHomePage : function(url){
+    return {
+        getHomePage : function(url){
 
-			var defer  = $q.defer();
+            var defer  = $q.defer();
 
-			$http.get(url).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
+            $http.get(url).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
 
-			return defer.promise
+            return defer.promise
 
-		},
-		getArticle : function(url,pg){
+        },
+        getArticle : function(url,pg){
 
-			var defer  = $q.defer(),
-				path   = this.removeHashFrag(url),
-				prefix = this.checkQueries(path),
-				page   = (pg) ? "&paged=" + pg : "",
-				query  = "json=1";
+            var defer  = $q.defer(),
+                path   = this.removeHashFrag(url),
+                prefix = this.checkQueries(path),
+                page   = (pg) ? "&paged=" + pg : "",
+                query  = "json=1";
 
-			//expose url for testing
-			defer.promise.url = path+prefix+query+page
+            //expose url for testing
+            defer.promise.url = path+prefix+query+page
 
-			$http.get(path+prefix+query+page).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
+            $http.get(path+prefix+query+page).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
 
-			return defer.promise
-		},
-		getPagedPosts : function(section,pg){
+            return defer.promise
+        },
+        getPagedPosts : function(section,pg){
 
-			var defer  = $q.defer(),
-				prefix = "/api/get_posts/?post_type=tls_articles&article_section=",
-				page   = (pg) ? "&paged=" + pg : "";
+            var defer  = $q.defer(),
+                prefix = "/api/get_posts/?post_type=tls_articles&article_section=",
+                page   = (pg) ? "&paged=" + pg : "";
 
-			//expose url for testing
-			defer.promise.url = prefix+section+page
+            //expose url for testing
+            defer.promise.url = prefix+section+page
 
-			$http.get(prefix+section+page).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
+            $http.get(prefix+section+page).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
 
-			return defer.promise
+            return defer.promise
 
-		},
-		getRelatedContent : function(tags){
+        },
+        getRelatedContent : function(tags){
 
-			var defer  = $q.defer(),
-				parse  = tags.toString(),
-				path1  = "/tag/"+parse+"/?json=1",
-				path2  = "/?tag="+parse+"&json=1",
-				url    = "/api/get_posts/?article_tags=" + parse;
+            var defer  = $q.defer(),
+                parse  = tags.toString(),
+                path1  = "/tag/"+parse+"/?json=1",
+                path2  = "/?tag="+parse+"&json=1",
+                url    = "/api/get_posts/?article_tags=" + parse;
 
-			//expose url for testing
-			defer.promise.url = url
-			$http.get(url).success(function (data){
+            //expose url for testing
+            defer.promise.url = url
+            $http.get(url).success(function (data){
 
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-				
-			})
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+                
+            })
 
-			return defer.promise
-		},
-		getSearchResults : function(path,page,filters,ord,date,contentFilters){
+            return defer.promise
+        },
+        getSearchResults : function(path,page,filters,ord,date,contentFilters) {
 
-			var defer     = $q.defer(),
-				page      = (!page) ? 1 : page,
-				filters   = (!filters) ? [] : filters,
-				converted = filters.toString().replace(/,/g,'&'),
-				filt      = (filters.length == 0) ? "" : "&"+converted,
-				prefix    = this.checkQueries(path),
-				order     = (!ord)? "" : "&orderby=date&order=" + ord,
-				dateRange = (!date)? "" : "&date_filter=" + date,
-				cFilters  = (!contentFilters)? "" : "&post_type["+contentFilters+"]",
-				query     = "json=1&paged="+page,
-				finalPath = path+prefix+query+filt+order+dateRange+cFilters;
+            var defer     = $q.defer(),
+                page      = (!page) ? 1 : page,
+                //filters   = (!filters) ? [] : filters,
+                //converted = 'article_section=' + filters.toString(),
+                //converted = filters.toString().replace(/,/g,'&'),
+                //filt      = (filters.length == 0) ? "" : "&"+converted,
+                filt      = (filters.length == 0) ? "" : "&"+filters,
+                prefix    = this.checkQueries(path),
+                order     = (!ord)? "" : "&orderby=date&order=" + ord,
+                dateRange = (!date)? "" : "&date_filter=" + date,
+                cFilters  = (!contentFilters)? "" : "&post_type["+contentFilters+"]",
+                query     = "json=1&paged="+page,
+                finalPath = path+prefix+query+filt+order+dateRange+cFilters;
 
-			//expose url for testing
-			defer.promise.url = finalPath
+            //expose url for testing
+            defer.promise.url = finalPath
+            console.log(finalPath);
 
-			$http.get(finalPath).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
-			return defer.promise
-		
-		},
-		getFullResults : function(path){
+            $http.get(finalPath).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
+            return defer.promise
+        
+        },
+        getFullResults : function(path){
 
-			var defer = $q.defer(),
-				prefix    = this.checkQueries(path),
-				query     = "json=1&count=-1",
-				finalPath = path+prefix+query;
+            var defer = $q.defer(),
+                prefix    = this.checkQueries(path),
+                query     = "json=1&count=-1",
+                finalPath = path+prefix+query;
 
-			//expose url for testing
-			defer.promise.url = finalPath
+            //expose url for testing
+            defer.promise.url = finalPath
 
-			$http.get(finalPath).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
-			return defer.promise
+            $http.get(finalPath).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
+            return defer.promise
 
-		},
-		getArticleList : function(page){
+        },
+        getArticleList : function(page){
 
-			var defer = $q.defer(),
-				path  = '/api/get_posts/?post_type=tls_articles&page=' + page;
+            var defer = $q.defer(),
+                path  = '/api/get_posts/?post_type=tls_articles&page=' + page;
 
-			$http.get(path).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
+            $http.get(path).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
 
-			return defer.promise
+            return defer.promise
 
-		},
-		getFaqs : function (){
+        },
+        getFaqs : function (){
 
-			var defer = $q.defer(),
-				path  = '/api/get_posts/?post_type=tls_faq';
+            var defer = $q.defer(),
+                path  = '/api/get_posts/?post_type=tls_faq';
 
-			$http.get(path).success(function (data){
-				//simulate server delay
-				$timeout(function(){
-					defer.resolve(data)
-				},delay)
-			})
+            $http.get(path).success(function (data){
+                //simulate server delay
+                $timeout(function(){
+                    defer.resolve(data)
+                },delay)
+            })
 
-			return defer.promise
-		},
-		checkQueries : function(url){
-			var prefix = (url.indexOf('?') > -1) ? "&" : "?"
-			return prefix
-		},
-		removeHashFrag : function(url){
+            return defer.promise
+        },
+        checkQueries : function(url){
+            var prefix = (url.indexOf('?') > -1) ? "&" : "?"
+            return prefix
+        },
+        removeHashFrag : function(url){
 
-			var hashIndex = url.indexOf('#'),
-				newPath   = url.slice(0,hashIndex);
+            var hashIndex = url.indexOf('#'),
+                newPath   = url.slice(0,hashIndex);
 
-			if(hashIndex > -1){
-				return newPath
-			} else{
-				return url
-			}
-			
-		}		
-	}
+            if(hashIndex > -1){
+                return newPath
+            } else{
+                return url
+            }
+            
+        }       
+    }
 }])
--- grunt/DEV/app/templates/search/search-controller.js
+++ grunt/DEV/app/templates/search/search-controller.js
@@ -1,222 +1,227 @@
 .controller('search',[
-	"$scope",
-	'$sce',
-	'$timeout',
-	'api',
-	'niceDate',
-	'tealium', 
-	function ($scope,$sce,$timeout,api,niceDate,tealium) {
+    "$scope",
+    '$sce',
+    '$timeout',
+    'api',
+    'niceDate',
+    'tealium', 
+    function ($scope,$sce,$timeout,api,niceDate,tealium) {
 
-	//Set default vars
-	var url = window.location.href;
-	$scope.filters         = []
-	$scope.taxonomyFilters = []
-	$scope.currentPage     = 1
-	$scope.dateRange       = ""
-	$scope.orderName       = "Newest"
-	$scope.order           = "DESC"
-	$scope.showSorter      = false
-	$scope.loadResults     = true
-	$scope.clearable       = false
-	$scope.niceDate        = niceDate
-
-
-	$scope.request = function(){
-
-		$scope.loadResults = true
-
-		api.getSearchResults(
-			url,
-			$scope.currentPage,
-			$scope.filters,
-			$scope.order,
-			$scope.dateRange
-			)
-			.then(function (results){
-			
-				$scope.showFilters = false
-				$scope.loadResults = false
-				$scope.results     = results
-				$scope.contentType = results.content_type_filters
-				$scope.sections    = results.articles_sections
-				$scope.dateRanges  = results.date_filters
-				$scope.paginationConfig = {
-					"pageCount"   : results.pages,
-					"currentPage" : $scope.currentPage,
-					"filters"     : $scope.filters,
-					"order"       : $scope.order,
-					"dateRange"   : $scope.dateRange
-			}
-
-				console.log(results);
-		})
-	}	
-
-	//Refresh scope when pagination page is selected
-	$scope.$on('loading',function (){
-		$scope.loadResults = true
-	})
-
-	$scope.$on('updatePage',function (e,results,curr){
-
-		$scope.loadResults = false
-		$scope.paginationConfig.currentPage = curr
-		$scope.currentPage = curr
-		$scope.results     = results
-
-	})
-
-	$scope.contentFilter = function(term,query,key,type){
-
-		console.log(type)
-
-		var list = (type == 'content') ? $scope.contentType : $scope.sections,
-			typeName = (type == 'content') ? 'content type' : type;
-
-		if(query){
-
-			var index = $scope.filters.indexOf(query);
-
-			if(index == -1){
-				$scope.filters.push(query)
-				list[key].isApplied = true
-				tealium.filtering('add',typeName,term);
-			} else {
-				$scope.filters.splice(index,1)
-				list[key].isApplied = false
-				tealium.filtering('remove',typeName,term);
-			}
-
-			$scope.loadResults = true
-			api.getSearchResults(
-					url,
-					1,
-					$scope.filters,
-					$scope.order,
-					$scope.dateRange
-				)
-				.then(function (results){
-				
-					$scope.loadResults = false
-					$scope.results = results
-					$scope.contentType = results.content_type_filters
-					$scope.sections    = results.articles_sections
-					$scope.dateRanges  = results.date_filters
-					$scope.paginationConfig = {
-						"pageCount"   : results.pages,
-						"currentPage" : 1,
-						"filters"     : $scope.filters,
-						"order"       : $scope.order,
-						"dateRange"   : $scope.dateRange
-				}
-			})
-		}
-	}
-
-	$scope.dateRangeFilter = function(range,name){
-
-		var $this = $scope.dateRanges[name]
-
-		if($this.isApplied){
-			$scope.dateRange = "";
-			$scope.clearable = false
-			tealium.filtering('remove','date',range);
-		} else {
-			$scope.dateRange = range;
-			$scope.clearable = true
-			tealium.filtering('add','date',range);
-		}
-
-		angular.forEach($scope.dateRanges, function (obj,val){
-			if(val != name){
-				obj.isApplied = false
-			}
-		});
-
-		$scope.dateRanges[name].isApplied = !$scope.dateRanges[name].isApplied
-		$scope.loadResults = true
-
-		api.getSearchResults(
-				url,
-				1,
-				$scope.filters,
-				$scope.order,
-				$scope.dateRange
-			)
-			.then(function (results){
-			
-				$scope.loadResults = false
-				$scope.results = results
-				$scope.contentType = results.content_type_filters
-				$scope.sections    = results.articles_sections
-				$scope.dateRanges  = results.date_filters
-				$scope.paginationConfig = {
-					"pageCount"   : results.pages,
-					"currentPage" : 1,
-					"filters"     : $scope.filters,
-					"order"       : $scope.order,
-					"dateRange"   : $scope.dateRange
-			}
-		})
-	}
-
-	$scope.orderResults = function(order,orderName){
-
-		$scope.loadResults = true
-
-		$scope.order     = order;
-		$scope.orderName = orderName;
-		
-		api.getSearchResults(
-				url,
-				1,
-				$scope.filters,
-				$scope.order,
-				$scope.dateRange)
-			.then(function (results){
-				
-				$scope.loadResults = false
-				$scope.results = results
-				$scope.contentType = results.content_type_filters
-				$scope.sections    = results.articles_sections
-				$scope.dateRanges  = results.date_filters
-				$scope.paginationConfig = {
-					"pageCount"   : results.pages,
-					"currentPage" : 1,
-					"filters"     : $scope.filters,
-					"order"       : $scope.order,
-					"dateRange"   : $scope.dateRange
-			}
-		})
-
-		tealium.sortOrder(orderName)
-	}
-
-	$scope.clearFilters = function(filters){
-
-		$scope.filters         = []
-		$scope.taxonomyFilters = []
-		$scope.currentPage     = 1
-		$scope.dateRange       = ""
-		$scope.clearable       = false
+        //Set default vars
+        var url = window.location.href;
+        $scope.filters         = []
+        $scope.activeFilters   = []
+        $scope.taxonomyFilters = []
+        $scope.currentPage     = 1
+        $scope.dateRange       = ""
+        $scope.orderName       = "Newest"
+        $scope.order           = "DESC"
+        $scope.showSorter      = false
+        $scope.loadResults     = true
+        $scope.clearable       = false
+        $scope.niceDate        = niceDate
 
 
-		angular.forEach($scope.contentType, function (obj){
-			obj.isApplied = false
-		});
+        // Helper function that checks whether a filter is active or not
+        $scope.inFiltersArray = function(value) {        	
+            var inFilter = false;
+            if ($scope.activeFilters.indexOf(value) != -1) inFilter = true;
+            return inFilter;
+        };
 
-		angular.forEach($scope.dateRanges, function (obj){
-			obj.isApplied = false
-		});
 
-		angular.forEach($scope.sections, function (obj){
-			obj.isApplied = false
-		});
+        $scope.request = function(){
 
-		$scope.request();
+            $scope.loadResults = true
 
-	}
+            api.getSearchResults(
+                url,
+                $scope.currentPage,
+                $scope.filters,
+                $scope.order,
+                $scope.dateRange
+                )
+                .then(function (results){
+                
+                    $scope.showFilters = false
+                    $scope.loadResults = false
+                    $scope.results     = results
+                    $scope.contentType = results.content_type_filters
+                    $scope.sections    = results.articles_sections
+                    $scope.dateRanges  = results.date_filters
+                    $scope.paginationConfig = {
+                        "pageCount"   : results.pages,
+                        "currentPage" : $scope.currentPage,
+                        "filters"     : $scope.filters,
+                        "order"       : $scope.order,
+                        "dateRange"   : $scope.dateRange
+                }
 
-	$scope.request();
+                //console.log(results);
+            })
+        }   
+
+        //Refresh scope when pagination page is selected
+        $scope.$on('loading',function (){
+            $scope.loadResults = true
+        })
+
+        $scope.$on('updatePage',function (e,results,curr){
+
+            $scope.loadResults = false
+            $scope.paginationConfig.currentPage = curr
+            $scope.currentPage = curr
+            $scope.results     = results
+
+        })
+
+        $scope.contentFilter = function(term,query,key,type) {
+
+            var list = (type == 'content') ? $scope.contentType : $scope.sections,
+                typeName = (type == 'content') ? 'content type' : type;
+
+            if (query) {
+
+                var index = $scope.filters.indexOf(query);
+
+                if (index == -1) {
+                    $scope.filters.push(query);
+                    // For active class
+                    $scope.activeFilters.push(key);
+                    tealium.filtering('add',typeName,term);
+                } else {
+                    $scope.filters.splice(index,1);
+                    $scope.activeFilters.splice(index,1);
+                    tealium.filtering('remove',typeName,term);
+                }
+
+                $scope.loadResults = true;              
+
+                // Refactor filters based on filter type
+                if (type == 'content') {                	
+                	var filtersArr   = (!$scope.filters) ? [] : $scope.filters;                	
+                	var converted = filtersArr.toString().replace(/,/g,'&');
+                	var filters = (filtersArr.length == 0) ? "" : "&"+converted;
+
+                } else if (type == 'category') {
+	                var filters = 'article_section=' + $scope.filters.toString();
+                }
+
+                api.getSearchResults(
+                        url,
+                        1,
+                        filters,
+                        $scope.order,
+                        $scope.dateRange
+                    )
+                    .then(function (results) {
+                    
+                        $scope.loadResults = false
+                        $scope.results = results
+                        $scope.contentType = results.content_type_filters
+                        $scope.sections    = results.articles_sections
+                        $scope.dateRanges  = results.date_filters
+                        $scope.paginationConfig = {
+                            "pageCount"   : results.pages,
+                            "currentPage" : 1,
+                            "filters"     : $scope.filters,
+                            "order"       : $scope.order,
+                            "dateRange"   : $scope.dateRange
+                        }   
+
+                        //console.log($scope.contentType);                    
+                })
+            }
+        }
+
+        $scope.dateRangeFilter = function(range,name){
+
+            var $this = $scope.dateRanges[name];
+            var index = $scope.activeFilters.indexOf(name);
+
+            if (index == -1) {
+                $scope.activeFilters.push(name);
+                $scope.dateRange = range;
+                $scope.clearable = true;
+                tealium.filtering('add','date',range);
+                
+            } else {
+                $scope.activeFilters.splice(index,1);
+                $scope.dateRange = "";
+                $scope.clearable = false
+                tealium.filtering('remove','date',range);
+            }
+
+            $scope.loadResults = true;
+
+            api.getSearchResults(
+                    url,
+                    1,
+                    $scope.filters,
+                    $scope.order,
+                    $scope.dateRange
+                )
+                .then(function (results){
+                
+                    $scope.loadResults = false
+                    $scope.results = results
+                    $scope.contentType = results.content_type_filters
+                    $scope.sections    = results.articles_sections
+                    $scope.dateRanges  = results.date_filters
+                    $scope.paginationConfig = {
+                        "pageCount"   : results.pages,
+                        "currentPage" : 1,
+                        "filters"     : $scope.filters,
+                        "order"       : $scope.order,
+                        "dateRange"   : $scope.dateRange
+                }
+            })
+        }
+
+        $scope.orderResults = function(order,orderName){
+
+            $scope.loadResults = true
+
+            $scope.order     = order;
+            $scope.orderName = orderName;
+            
+            api.getSearchResults(
+                    url,
+                    1,
+                    $scope.filters,
+                    $scope.order,
+                    $scope.dateRange)
+                .then(function (results){
+                    
+                    $scope.loadResults = false
+                    $scope.results = results
+                    $scope.contentType = results.content_type_filters
+                    $scope.sections    = results.articles_sections
+                    $scope.dateRanges  = results.date_filters
+                    $scope.paginationConfig = {
+                        "pageCount"   : results.pages,
+                        "currentPage" : 1,
+                        "filters"     : $scope.filters,
+                        "order"       : $scope.order,
+                        "dateRange"   : $scope.dateRange
+                }
+            })
+
+            tealium.sortOrder(orderName)
+        }
+
+        $scope.clearFilters = function(filters){
+
+            $scope.filters         = [];
+            $scope.activeFilters   = [];
+            $scope.taxonomyFilters = [];
+            $scope.currentPage     = 1;
+            $scope.dateRange       = "";
+            $scope.clearable       = false;
+
+            $scope.request();
+        }
+
+        $scope.request();
 
 }])
