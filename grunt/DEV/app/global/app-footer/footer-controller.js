f2e3ad47a01f1e9d6f40ee9452ac5529b847c714
Silvia Rebelo <Silvia.Rebelo@enginegroup.com>
2015-05-15 15:41:05 +0100

    NUTLS-171 Facebook and Twitter links in the footer open in the same tab

--- grunt/DEV/app/global/app-footer/footer-controller.js
+++ grunt/DEV/app/global/app-footer/footer-controller.js
@@ -6,6 +6,16 @@
 		tealium.classified(pdf);
 	}
 
+	$scope.exitLink = function(type, url) {
+		
+		tealium.exitLink(type);
+		
+		// If url is passed then redirect page to that page				
+		if (url) {					
+			$window.open(url,'_blank');
+		}
+	}
+
 	$scope.archive = function(url) {
 
 		tealium.archive();
