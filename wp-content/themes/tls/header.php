9dc40d7d449aa7f5ae4360732813ac653046b860
Vitor Faiante <Vitor.Faiante@madebypartners.com>
2015-05-21 18:26:39 +0100

    - added TLS favicon using http://realfavicongenerator.net/

--- wp-content/themes/tls/header.php
+++ wp-content/themes/tls/header.php
@@ -26,6 +26,21 @@
 		</script>
 	<?php endif; ?>
 
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo TLS_THEME_URI; ?>/images/icons/apple-touch-icon-144x144.png">
+    <link rel="icon" type="image/png" href="<?php echo TLS_THEME_URI; ?>/images/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo TLS_THEME_URI; ?>/images/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php echo TLS_THEME_URI; ?>/images/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php echo TLS_THEME_URI; ?>/images/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="<?php echo TLS_THEME_URI; ?>/images/icons/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
 </head>
 
 <body <?php body_class(); ?> tls-window-size="size">
